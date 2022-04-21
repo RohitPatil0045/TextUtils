6407a8ac88f780f3bc023f520fb14b08fa103401
ROHITPATIL0045 <rohitpatil0036@gmail.com>
2022-04-21 16:53:45 +0530

    Configure TextUtils app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
+import React, { useState } from "react";
+import Alert from "./component/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const changeMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#202124";
+      showAlert("Dark Mode is ON", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is ON", "success");
+    }
+  };
+
+
+  const showAlert = (massage, type) => {
+    setAlert({
+      massage: massage,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggle={changeMode} />
+      <Alert alert={alert} />
+      <TextForm mode={mode} showAlert={showAlert} />
+    </>
   );
 }
 
